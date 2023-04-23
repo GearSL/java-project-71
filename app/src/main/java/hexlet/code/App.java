19ff1ee125df5f6a8875c7619081afbc0c080251
i.ismailov <i.ismailov@dengiclick.kz>
2023-04-23 21:33:52 +0600

    Added picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,27 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    public static Boolean help = false;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    public static Boolean version = false;
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+
         System.out.println("Hello, world!");
+    }
+
+    @Override
+    public Integer call(){
+        return null;
     }
 }
