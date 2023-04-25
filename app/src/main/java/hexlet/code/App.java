82680d9afd8969b3b90e44bc4523a1fc1e8e2663
i.ismailov <i.ismailov@dengiclick.kz>
2023-04-25 17:39:24 +0600

    Added two parameters for file path and a small adaptation to the 4th step requirements

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,16 +2,22 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    public static Boolean help = false;
-
+    private static Boolean help = false;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    public static Boolean version = false;
+    private static Boolean version = false;
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private static String format = "stylish";
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private static String filepath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private static String filepath2;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
