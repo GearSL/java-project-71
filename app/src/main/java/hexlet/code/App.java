b5ed3e65534d6c3cd9f9242203a5f835e3547c80
i.ismailov <i.ismailov@dengiclick.kz>
2023-04-26 19:29:05 +0600

    Added first functionality to compare two flat json files and file examples for test

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,17 +3,20 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    private static Boolean help = false;
+    private final static Boolean help = false;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    private static Boolean version = false;
+    private final static Boolean version = false;
     @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
-    private static String format = "stylish";
+    private final static String format = "stylish";
     @Parameters(paramLabel = "filepath1", description = "path to first file")
     private static String filepath1;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
@@ -27,7 +30,22 @@ public class App implements Callable<Integer> {
     }
 
     @Override
-    public Integer call(){
+    public Integer call() throws Exception {
+        Path file1Path = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path file2Path = Paths.get(filepath2).toAbsolutePath().normalize();
+
+        if (!Files.exists(file1Path)) {
+            throw new Exception("File '" + file1Path + "' does not exist");
+        }
+        if (!Files.exists(file2Path)) {
+            throw new Exception("File '" + file2Path + "' does not exist");
+        }
+
+        String contentFile1 = Files.readString(file1Path);
+        String contentFile2 = Files.readString(file2Path);
+
+        System.out.println(Differ.generate(contentFile1, contentFile2));
+
         return null;
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,31 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.SortedMap;
+
+public class Differ {
+    public static String generate(String file1, String file2) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        SortedMap<String, Object> firstJsonMap = mapper.readValue(file1, new TypeReference<>(){});
+        SortedMap<String, Object> secondJsonMap = mapper.readValue(file2, new TypeReference<>(){});
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{\n");
+        for (String key : firstJsonMap.keySet()) {
+            if (secondJsonMap.containsKey(key) && secondJsonMap.get(key).equals(firstJsonMap.get(key))) {
+                stringBuilder.append("   ").append(key).append(": ").append(firstJsonMap.get(key)).append("\n");
+            } else if (secondJsonMap.containsKey(key) && !secondJsonMap.get(key).equals(firstJsonMap.get(key))) {
+                stringBuilder.append(" - ").append(key).append(": ").append(firstJsonMap.get(key)).append("\n");
+                stringBuilder.append(" + ").append(key).append(": ").append(secondJsonMap.get(key)).append("\n");
+            } else if (!secondJsonMap.containsKey(key)) {
+                stringBuilder.append(" - ").append(key).append(": ").append(firstJsonMap.get(key)).append("\n");
+            }
+        }
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
+    }
+}
