3b5218abe17edc3c3f877a0b5b9beb686751e7a7
i.ismailov <i.ismailov@dengiclick.kz>
2023-05-21 16:27:04 +0600

    review #1: attempt to fix hexlet tests

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -10,24 +10,19 @@ public class Plain {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (Diff diffEl : diff) {
-            switch (diffEl.getState()) {
-                case "removed" -> {
-                    String diffString = String.format("Property '%s' was %s\n", diffEl.getKey(), diffEl.getState());
-                    stringBuilder.append(diffString);
-                }
-                case "added" -> {
-                    String diffString = String.format("Property '%s' was %s with value: %s\n",
-                            diffEl.getKey(), diffEl.getState(), plainValueFormat(diffEl.getNewValue()));
-                    stringBuilder.append(diffString);
-                }
-                case "updated" -> {
-                    String diffString = String.format("Property '%s' was %s. From %s to %s\n",
-                            diffEl.getKey(), diffEl.getState(),
-                            plainValueFormat(diffEl.getOldValue()), plainValueFormat(diffEl.getNewValue()));
-                    stringBuilder.append(diffString);
-                }
-                case "unchanged" -> System.out.println("We are not doing anything for this state");
-                default -> throw new RuntimeException("The status is wrong: " + diffEl.getState());
+            String state = diffEl.getState();
+            if (state.equals("removed")) {
+                String diffString = String.format("Property '%s' was %s\n", diffEl.getKey(), diffEl.getState());
+                stringBuilder.append(diffString);
+            } else if (state.equals("added")) {
+                String diffString = String.format("Property '%s' was %s with value: %s\n",
+                        diffEl.getKey(), diffEl.getState(), plainValueFormat(diffEl.getNewValue()));
+                stringBuilder.append(diffString);
+            } else if (state.equals("updated")) {
+                String diffString = String.format("Property '%s' was %s. From %s to %s\n",
+                        diffEl.getKey(), diffEl.getState(),
+                        plainValueFormat(diffEl.getOldValue()), plainValueFormat(diffEl.getNewValue()));
+                stringBuilder.append(diffString);
             }
         }
         return stringBuilder.toString().trim();
